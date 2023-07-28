be644652b81355b1b39458ce1231093de9e014cb
muhammad.askar <muhammad.askar@indocyber.id>
2023-07-28 22:12:33 +0700

    refactor: allow origin

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	fmt.Println(port)
 	fmt.Println(ORIGIN_PROD + "\n" + ORIGIN_DEV)
 
-	config.AllowOrigins = []string{"http://38.47.69.131:2000"}
+	config.AllowOrigins = []string{"http://38.47.69.131:2000", "http://127.0.0.1:3000"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Authorization", "Content-Type", "Access-Control-Allow-Origin"}
 	config.AllowCredentials = true
