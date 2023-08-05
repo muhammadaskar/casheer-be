c74a67e2b0d55f7d25150f04e105fd5d1c438a61
muhammad.askar <muhammad.askar@indocyber.id>
2023-08-05 15:52:57 +0700

    fix: cors

--- app/routes/router.go
+++ app/routes/router.go
@@ -100,14 +100,10 @@ func NewRouter() *gin.Engine {
 	api.POST("/auth/register", userHandler.Register)
 	api.POST("/auth/login", userHandler.Login)
 
-	categoryRouter := api.Group("category")
-	categoryRouter.Use(cors.New(config))
-
-	categoryRouter.GET("/", authMiddleware, categoryHandler.FindAll)
-	categoryRouter.GET("/:id", authMiddleware, categoryHandler.FindById)
-	categoryRouter.POST("/", authAdminMiddleware, categoryHandler.Create)
-	categoryRouter.PUT("/:id", authAdminMiddleware, categoryHandler.Update)
-
+	api.GET("/category", authMiddleware, categoryHandler.FindAll)
+	api.GET("/category/:id", authMiddleware, categoryHandler.FindById)
+	api.POST("/category", authAdminMiddleware, categoryHandler.Create)
+	api.PUT("/category/:id", authAdminMiddleware, categoryHandler.Update)
 	api.GET("/notification", authAdminMiddleware, notificationHandler.FindAll)
 
 	return router
