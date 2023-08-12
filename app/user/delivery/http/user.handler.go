2878f72b40603828be35649ab30b284bc000bb33
muhammad.askar <muhammad.askar@indocyber.id>
2023-08-13 03:25:40 +0700

    refactor custom reponse and use in user handler

--- app/user/delivery/http/user.handler.go
+++ app/user/delivery/http/user.handler.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/muhammadaskar/casheer-be/app/helper"
 	"github.com/muhammadaskar/casheer-be/app/user"
 	"github.com/muhammadaskar/casheer-be/app/user/usecase"
 	"github.com/muhammadaskar/casheer-be/infrastructures/auth"
+	customresponse "github.com/muhammadaskar/casheer-be/utils/custom_response"
 )
 
 type UserHandler struct {
@@ -25,10 +25,10 @@ func (h *UserHandler) Register(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		errors := helper.FormatValidationError(err)
+		errors := customresponse.FormatValidationError(err)
 		errMessage := gin.H{"errors": errors}
 
-		response := helper.APIResponse(message, http.StatusUnprocessableEntity, "error", errMessage)
+		response := customresponse.APIResponse(message, http.StatusUnprocessableEntity, "error", errMessage)
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
@@ -36,19 +36,19 @@ func (h *UserHandler) Register(c *gin.Context) {
 	newUser, err := h.userUseCase.Register(input)
 	if err != nil {
 		errMessage := gin.H{"errors": err.Error()}
-		response := helper.APIResponse(message, http.StatusUnprocessableEntity, "error", errMessage)
+		response := customresponse.APIResponse(message, http.StatusUnprocessableEntity, "error", errMessage)
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
 
 	if err != nil {
-		response := helper.APIResponse(message, http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse(message, http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	formatter := user.FormatUser(newUser, "")
-	response := helper.APIResponse("Account has been registered, admin will active your account", http.StatusCreated, "success", formatter)
+	response := customresponse.APIResponse("Account has been registered, admin will active your account", http.StatusCreated, "success", formatter)
 	c.JSON(http.StatusCreated, response)
 }
 
@@ -57,9 +57,9 @@ func (h *UserHandler) Login(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		errors := helper.FormatValidationError(err)
+		errors := customresponse.FormatValidationError(err)
 		errMessage := gin.H{"errors": errors}
-		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errMessage)
+		response := customresponse.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errMessage)
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
@@ -67,20 +67,20 @@ func (h *UserHandler) Login(c *gin.Context) {
 	loggedInUser, err := h.userUseCase.Login(input)
 	if err != nil {
 		errMessage := gin.H{"errors": err.Error()}
-		response := helper.APIResponse("Login Failed", http.StatusBadRequest, "error", errMessage)
+		response := customresponse.APIResponse("Login Failed", http.StatusBadRequest, "error", errMessage)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	token, err := h.authJwt.GenerateToken(loggedInUser.ID, loggedInUser.Email, loggedInUser.Role)
 	if err != nil {
-		reponse := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+		reponse := customresponse.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, reponse)
 		return
 	}
 
 	formatter := user.FormatUser(loggedInUser, token)
-	response := helper.APIResponse("Successfully loggedin", http.StatusOK, "success", formatter)
+	response := customresponse.APIResponse("Successfully loggedin", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 	return
 }
