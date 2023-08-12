3c9852fdb414071cf4aa9794e1161f6aa72d5134
muhammad.askar <muhammad.askar@indocyber.id>
2023-08-13 04:02:18 +0700

    refactor: custom reponse and use in category handler

--- app/category/delivery/http/category.handler.go
+++ app/category/delivery/http/category.handler.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/muhammadaskar/casheer-be/app/category"
 	"github.com/muhammadaskar/casheer-be/app/category/usecase"
-	"github.com/muhammadaskar/casheer-be/app/helper"
+	customresponse "github.com/muhammadaskar/casheer-be/utils/custom_response"
 )
 
 type CategoryHandler struct {
@@ -20,12 +20,12 @@ func NewCategoryHandler(categoryUseCase usecase.CategoryUseCase) *CategoryHandle
 func (h *CategoryHandler) FindAll(c *gin.Context) {
 	categories, err := h.categoryUseCase.FindAll()
 	if err != nil {
-		response := helper.APIResponse("Error to get categories", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("Error to get categories", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("List of categories", http.StatusOK, "success", category.FormatCategories(categories))
+	response := customresponse.APIResponse("List of categories", http.StatusOK, "success", category.FormatCategories(categories))
 	c.JSON(http.StatusOK, response)
 }
 
@@ -34,19 +34,19 @@ func (h *CategoryHandler) FindById(c *gin.Context) {
 
 	err := c.ShouldBindUri(&input)
 	if err != nil {
-		response := helper.APIResponse("failed to get detail of category", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("failed to get detail of category", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	getCategory, err := h.categoryUseCase.FindById(input)
 	if err != nil {
-		response := helper.APIResponse(err.Error(), http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse(err.Error(), http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("Detail of category", http.StatusOK, "success", category.FormatCategory(getCategory))
+	response := customresponse.APIResponse("Detail of category", http.StatusOK, "success", category.FormatCategory(getCategory))
 	c.JSON(http.StatusOK, response)
 }
 
@@ -55,19 +55,19 @@ func (h *CategoryHandler) Create(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		response := helper.APIResponse("Failed to create category", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("Failed to create category", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	newCategory, err := h.categoryUseCase.Create(input)
 	if err != nil {
-		response := helper.APIResponse("Failed to create category", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("Failed to create category", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("category created successfully", http.StatusCreated, "success", category.FormatCategory(newCategory))
+	response := customresponse.APIResponse("category created successfully", http.StatusCreated, "success", category.FormatCategory(newCategory))
 	c.JSON(http.StatusCreated, response)
 }
 
@@ -76,7 +76,7 @@ func (h *CategoryHandler) Update(c *gin.Context) {
 
 	err := c.ShouldBindUri(&inputID)
 	if err != nil {
-		response := helper.APIResponse("Failed to update cateogry", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("Failed to update cateogry", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
@@ -84,21 +84,21 @@ func (h *CategoryHandler) Update(c *gin.Context) {
 	var inputData category.CreateCategoryInput
 	err = c.ShouldBindJSON(&inputData)
 	if err != nil {
-		errors := helper.FormatValidationError(err)
+		errors := customresponse.FormatValidationError(err)
 		errorMessage := gin.H{"errors": errors}
 
-		response := helper.APIResponse("Failed to update category", http.StatusUnprocessableEntity, "error", errorMessage)
+		response := customresponse.APIResponse("Failed to update category", http.StatusUnprocessableEntity, "error", errorMessage)
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
 
 	updateCategory, err := h.categoryUseCase.UpdateCategory(inputID, inputData)
 	if err != nil {
-		response := helper.APIResponse("Failed to update category", http.StatusBadRequest, "error", nil)
+		response := customresponse.APIResponse("Failed to update category", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("Success to update category", http.StatusOK, "success", category.FormatCategory(updateCategory))
+	response := customresponse.APIResponse("Success to update category", http.StatusOK, "success", category.FormatCategory(updateCategory))
 	c.JSON(http.StatusOK, response)
 }
