a7dd224707c1c6b8dfcaadbf06f8dcc369c55af6
Muhammad Askar <48124631+muhammadaskar@users.noreply.github.com>
2023-09-10 21:44:18 +0700

    refactor: change conf

--- infrastructures/mysql_driver/mysql_driver.go
+++ infrastructures/mysql_driver/mysql_driver.go
@@ -22,12 +22,12 @@ func InitDatabase() *gorm.DB {
 
 	// Access environment variables
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	//dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
